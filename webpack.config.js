82f8b9b041f11da8ac1df4a169b9d3ebdf7d654d
rentao <rentao@baijiahulian.com>
2021-01-25 17:39:20 +0800

    feat: del

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const { merge } = require('webpack-merge');
 const base = require('../../webpack.config.base');
 const commonId = require('../../webpack-plugin/commonId.js');
 const {url, name} = require('./config');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 module.exports = merge(
   {
     entry: './index.js',
@@ -10,7 +11,7 @@ module.exports = merge(
       path: path.join(__dirname, './lib'),
       filename: '[name][hash].js',
     },
-    plugins: [new (commonId(url, name))()],
+    plugins: [new (commonId(url, name))(), new CleanWebpackPlugin(),],
     externals: ['@feitu/imis']
   },
   base,
